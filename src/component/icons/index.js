d6a081ff80cb5c92e3dc3f90c93bcad9fe516dcc
jbgreulich <jessica@greulich.me>
2020-02-27 21:02:41 -0500

    add/edit icons

--- src/component/icons/index.js
+++ src/component/icons/index.js
@@ -4,9 +4,14 @@ import couchIconAbout from './couchdb-icon.svg';
 import couchIconModal from './couchdb-icon-modal.svg';
 import mochaIconAbout from './mocha.svg';
 import mochaIconModal from './mocha-modal.svg';
+import postgresqlIconAbout from './postgresql.svg';
+import railsIconAbout from './rails.svg';
+import reduxIconAbout from './redux.svg';
+import reduxIconModal from './redux-modal.svg';
+import rubyIconAbout from './ruby.svg';
 import sqliteIconAbout from './sqlite.svg';
 import sqliteIconModal from './sqlite-modal.svg';
 import webpackIconAbout from './webpack.svg';
 import webpackIconModal from './webpack-modal.svg';
 
-export {babelIconAbout, babelIconModal, couchIconAbout, couchIconModal, mochaIconAbout, mochaIconModal, sqliteIconAbout, sqliteIconModal, webpackIconAbout, webpackIconModal};
+export {babelIconAbout, babelIconModal, couchIconAbout, couchIconModal, mochaIconAbout, mochaIconModal, postgresqlIconAbout, railsIconAbout, reduxIconAbout, reduxIconModal, rubyIconAbout, sqliteIconAbout, sqliteIconModal, webpackIconAbout, webpackIconModal};
